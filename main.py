a31e4970e208b1079f0717c92f792cce49704669
NTworker <mikky20101@gmail.com>
2026-02-24 12:42:05 +0300

    Added error handling and weather type output, and changed the styling

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import requests
 from rich.console import Console
 from rich.panel import Panel
 import json
+from rich import box
+import weather_types
+from weather_types import weather_type_info
 
 console = Console()
 
 header = Panel(
     "Введи свой город:",
-    title="Weather CLI",
+    title="[bold]Weather CLI[/bold]",
     border_style='blue',
+    box=box.ROUNDED,
 )
 
 console.print(header)
@@ -26,23 +30,33 @@ temperature_data = round(weather_data['main']['temp'])
 temperature_feel_data = round(weather_data['main']['feels_like'])
 humidity_data = round(weather_data['main']['humidity'])
 wind_speed_data = round(weather_data['wind']['speed'])
+weather_data = weather_data['weather'][0]['main']
 
-def display_weather(temperature, temperature_feel, humidity, wind_speed):
+def display_weather(temperature, temperature_feel, humidity, wind_speed, weather):
+    try:
 
-    content = f"""
-    [bold yellow]🌡️ Температура: {temperature}°C[/bold yellow]
-    [bold yellow]ℹ️ Ощущается: {temperature_feel}°C[/bold yellow]
-    [bold yellow]💧 Влажность: {humidity}%[/bold yellow]
-    [bold yellow]💨 Скорость ветра: {wind_speed} м/c[/bold yellow]
-"""
+        info = weather_type_info[weather]
 
-    result = Panel(
-        content,
-        title=f"Погода в {city} прямо сейчас",
-        border_style='cyan',
-    )
+        content = f"""
+            {info["emoji"]} [bold blue]{info["description"]}[/bold blue]
+        
+            🌡️[yellow] Температура: [yellow][magenta]{temperature}°C[magenta]
+            ℹ️[yellow] Ощущается: [yellow][magenta]{temperature_feel}°C[magenta]
+            💧[yellow] Влажность: [yellow][magenta]{humidity}%[magenta]
+            💨[yellow] Скорость ветра: [yellow][magenta]{wind_speed} м/c[magenta]
+    """
 
-    console.print(result)
+        result = Panel(
+            content,
+            title=f"Погода в {city} прямо сейчас",
+            border_style='cyan',
+            box=box.ROUNDED,
+        )
 
-display_weather(temperature_data, temperature_feel_data, humidity_data, wind_speed_data)
+        console.print(result)
+    except Exception as e:
+        print(f'Произошла ошибка {e}')
+    finally:
+        console.print("[italic blue]Хорошего дня![/italic blue]")
 
+display_weather(temperature_data, temperature_feel_data, humidity_data, wind_speed_data, weather_data)
