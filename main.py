3674d71c2a161b7580f5670aabd6114202d85d76
NTworker <mikky20101@gmail.com>
2026-02-25 22:21:38 +0300

    Fixed error handling and improved performance

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import requests
 from rich.console import Console
 from rich.panel import Panel
-import json
 from rich import box
-import weather_types
 from weather_types import weather_type_info
+from requests.exceptions import ConnectionError, ConnectTimeout, HTTPError
 
 console = Console()
 
@@ -20,43 +19,59 @@ console.print(header)
 console.print("[bold green]> [/bold green]", end="")
 city = input()
 
+url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
 
-url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
+weather_data = {}
 
-weather_data = requests.get(url).json()
-weather_data_structure = json.dumps(weather_data, indent=2)
+try:
+    weather_data = requests.get(url).json()
+except requests.exceptions.ConnectionError:
+    print('Нет соединения!')
+except requests.exceptions.ConnectTimeout:
+    print("Вы отключены от сервера!")
+except requests.exceptions.HTTPError:
+    print("Сетевая ошибка!")
 
-temperature_data = round(weather_data['main']['temp'])
-temperature_feel_data = round(weather_data['main']['feels_like'])
-humidity_data = round(weather_data['main']['humidity'])
-wind_speed_data = round(weather_data['wind']['speed'])
-weather_data = weather_data['weather'][0]['main']
+def display_weather(weather: dict[str, dict]) -> None:
+    temperature = ""
+    temperature_feel = ""
+    humidity = ""
+    wind_speed = ""
 
-def display_weather(temperature, temperature_feel, humidity, wind_speed, weather):
     try:
 
-        info = weather_type_info[weather]
+        temperature = round(weather['main']['temp'])
+        temperature_feel = round(weather['main']['feels_like'])
+        humidity = round(weather['main']['humidity'])
+        wind_speed = round(weather['wind']['speed'])
 
-        content = f"""
-            {info["emoji"]} [bold blue]{info["description"]}[/bold blue]
-        
-            🌡️[yellow] Температура: [yellow][magenta]{temperature}°C[magenta]
-            ℹ️[yellow] Ощущается: [yellow][magenta]{temperature_feel}°C[magenta]
-            💧[yellow] Влажность: [yellow][magenta]{humidity}%[magenta]
-            💨[yellow] Скорость ветра: [yellow][magenta]{wind_speed} м/c[magenta]
-    """
+        info = weather_type_info[weather['weather'][0]['main']]
 
-        result = Panel(
-            content,
-            title=f"Погода в {city} прямо сейчас",
-            border_style='cyan',
-            box=box.ROUNDED,
-        )
-
-        console.print(result)
+    except KeyError:
+        print(f'Ошибка данных')
     except Exception as e:
-        print(f'Произошла ошибка {e}')
-    finally:
-        console.print("[italic blue]Хорошего дня![/italic blue]")
+        print(f"Произошла ошибка {e}")
 
-display_weather(temperature_data, temperature_feel_data, humidity_data, wind_speed_data, weather_data)
+    info = weather_type_info[weather['weather'][0]['main']]
+
+    content = f"""
+                {info["emoji"]} [bold blue]{info["description"]}[/bold blue]
+
+                🌡️[yellow] Температура: [yellow][magenta]{temperature}°C[magenta]
+                ℹ️[yellow] Ощущается: [yellow][magenta]{temperature_feel}°C[magenta]
+                💧[yellow] Влажность: [yellow][magenta]{humidity}%[magenta]
+                💨[yellow] Скорость ветра: [yellow][magenta]{wind_speed} м/c[magenta]
+        """
+
+    result = Panel(
+        content,
+        title=f"Погода в {city} прямо сейчас",
+        border_style='cyan',
+        box=box.ROUNDED,
+    )
+
+    console.print(result)
+    console.print("[italic blue]Хорошего дня![/italic blue]")
+
+
+display_weather(weather_data)
